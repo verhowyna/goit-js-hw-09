6e04e1906b6bb59a954ae0d07d945bddd7c3ed26
verhowyna <verhowyna@ukr.net>
2023-09-24 18:21:31 +0300

    second task-1

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,23 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+import { convertMs } from "./helpers";
+
+import { refs } from "./refs";
+
+btnStart.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      if (selectedDates[0] < Date.now()) {
+        alert('Please choose a date in the future')
+      } else {
+          btnStart.disabled = false;
+    }
+  },
+};
+
