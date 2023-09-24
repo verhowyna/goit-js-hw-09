a9ea3ed523992563c7c08d470d3cec6cf1696c5b
verhowyna <verhowyna@ukr.net>
2023-09-24 21:10:27 +0300

    second task-3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,7 +3,12 @@ import "flatpickr/dist/flatpickr.min.css";
 
 import { convertMs } from "./helpers";
 
-import { refs } from "./refs";
+const timerDays = document.querySelector('[data-days]');
+const timerHours = document.querySelector('[data-hours]');
+const timerMinutes = document.querySelector('[data-minutes]');
+const timerSeconds = document.querySelector('[data-seconds]');
+const inputForm = document.querySelector('#datetime-picker');
+const btnStart = document.querySelector('[data-start]');
 
 btnStart.disabled = true;
 
@@ -21,3 +26,25 @@ const options = {
   },
 };
 
+btnStart.addEventListener('click', onStart);
+
+function onStart(evt) {
+  btnStart.disabled = true;
+  inputForm.disabled = true;
+  const id = setInterval(() => {
+    const currentDate = Date.now();
+    const selectedDate = new Date(inputForm.value);
+    const dif = selectedDate - currentDate;
+    if (dif < 1000) {
+      clearInterval(id);
+    }
+    const timer = convertMs(dif);
+    timerDays.textContent = timer.days.toString().padStart(2, 0);
+    timerHours.textContent = timer.hours.toString().padStart(2, 0);
+    timerMinutes.textContent = timer.minutes.toString().padStart(2, 0);
+    timerSeconds.textContent = timer.seconds.toString().padStart(2, 0);
+  }, 1000)
+}
+
+
+flatpickr(inputForm, options);
--- src/js/refs.js
+++
@@ -1,8 +0,0 @@
-export const refs = {
-    timerDays: document.querySelector('[data-days]'),
-    timerHours: document.querySelector('[data-hours]'),
-    timerMinutes: document.querySelector('[data-minutes]'),
-    timerSeconds: document.querySelector('[data-seconds]'),
-    inputForm: document.querySelector('#datetime-picker'),
-    btnStart: document.querySelector('[data-start]'),
-}
