48c13b17ea4918b3e7eeb5a73b2765fc9b807b61
verhowyna <verhowyna@ukr.net>
2023-09-25 22:42:44 +0300

    monday-3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -44,8 +44,7 @@ function onStart(evt) {
     timerHours.textContent = timer.addLeadingZero(hours);
     timerMinutes.textContent = timer.addLeadingZero(minutes);
     timerSeconds.textContent = timer.addLeadingZero(seconds);
-    addLeadingZero();
-
+    
   }, 1000)
 }
 flatpickr(inputForm, options);
