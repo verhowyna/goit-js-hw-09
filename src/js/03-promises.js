340345fdd884efa92e4433a3fa55b0c5733927bf
verhowyna <verhowyna@ukr.net>
2023-09-27 22:28:42 +0300

    third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
-function createPromise(position, delay) {
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const inputDate = {};
+
+const form = document.querySelector('.form');
+
+form.addEventListener('input', onInput);
+form.addEventListener('submit', onSubmit);
+
+function onInput(evt) {
+if (evt.target.name === 'delay') {
+    inputDate.delay = +(evt.target.value);
+}
+if (evt.target.name === 'step') {
+  inputDate.step = +(evt.target.value);
+}
+if (evt.target.name === 'amount') {
+  inputDate.amount = +(evt.target.value);
+}
+}
+
+function onSubmit(evt) {
+evt.preventDefault();
+
+for (let i = 0; i < inputDate.amount; i += 1){
+const delayStep = inputDate.delay + inputDate.step * i;
+
+createPromise(i + 1, delayStep)
+  .then(({ position, delay }) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+}
+}
+
+function createPromise(positionDate, delayDate) {
+  return new Promise((resolve, reject) => {
+    setTimeout((delay) => {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  resolve({position: positionDate, delay: delayDate})
+  } 
+    reject({position: positionDate, delay: delayDate})
+  }, delayDate)
+})
 }
